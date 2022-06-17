590959427c16158bd33af1603e2e8cc0170d2fab
Madesh S <madesh.sampangi@gmail.com>
2022-06-17 23:26:57 +0530

    Online Course Registration

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Course Regitration System :: Home Page</title>
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//Meta tag Keywords -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/><!--stylesheet-->
+	<link rel="stylesheet" href="css/font-awesome.css"><!--font_aswesome_icons-->
+	<link href="//fonts.googleapis.com/css?family=Basic" rel="stylesheet"><!--online-fonts-->
+	<link href="//fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"><!--online-fonts-->
+</head>
+<body>
+	<div class="w3ls-icons">
+		<ul style="font-size: 30px">
+			<li><a href="onlinecourse/admin" style="color: #fff;"> ADMIN</a></li>
+			<li style="padding-left: 20px"><a href="onlinecourse" style="color: #fff;"> USER</a></li>
+		</ul>
+	</div>
+<div class="w3ls-head">
+		<h1>Online Course Registration</h1>
+</div>
+<div class="w3ls-content">
+	<div class="w3ls-headding">
+		<h2> <img src="images/bg3.png"></h2>
+		<p>We are launching the most awesome site ever. It's gonna be legendary</p>
+	</div>
+	
+</div>
+
+<footer>&copy; 2022 Online  Course Registration</footer>
+</body>
+</html>
